a3497607bea1b9d7872da0dfdeaefd14be737f91
Arya Girish K <arya.girish.k@ibm.com>
2026-02-19 19:02:45 +0530

    testing

--- scripts/install-binaries.sh
+++ scripts/install-binaries.sh
@@ -44,7 +44,7 @@ install_jq "latest" "${DIRECTORY}" "true" || true
 echo "Installing ibmcloud."
 install_ibmcloud "latest" "${DIRECTORY}" "true" || true
 echo "Installing ibmcloud code engine plugin."
-install_ibmcloud_plugin "code-engine" "latest" "${DIRECTORY}" "true" || true
+install_ibmcloud_plugins "code-engine" "latest" "${DIRECTORY}" "true" || true
 
 rm -rf "$TMP_DIR"
 
